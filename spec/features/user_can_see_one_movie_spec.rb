7a50b91055394d0dc5b4ef8c46ccb6c3b65c7eaf
Ellen Cornelius <ecorneli@iwu.edu>
2018-02-27 11:00:14 -0700

    Add year on joins table

--- spec/features/user_can_see_one_movie_spec.rb
+++ spec/features/user_can_see_one_movie_spec.rb
@@ -21,6 +21,8 @@ describe "user sees one movie's award information" do
     @award_2 = create(:award, title: "Best Sound")
     @award_3 = create(:award, title: "Best Action Film")
     @movie_2.awards << [@award_1, @award_2, @award_3]
+    @best_picture_guardians = MovieAward.find_by(award_id: @award_1.id, movie_id: @movie_2.id)
+    @best_picture_guardians.year = 1991
   end
   describe "user links to specific movie from movies index page" do
     it "user sees Best Picture, Best Sound, Best Action Film under an Awards for this Movie heading and ssees what year the award was won" do
@@ -28,7 +30,10 @@ describe "user sees one movie's award information" do
       click_on "Guardians of the Galaxy"
 
       expect(current_path).to eq(movie_path(@movie_2.slug))
-      expect(page).to have_content()
+      expect(page).to have_content(@award_1.title)
+      expect(page).to have_content(@award_2.title)
+      expect(page).to have_content(@award_3.title)
+      expect(page).to have_content(@best_picture_guardians.year)
     end
   end
 end
